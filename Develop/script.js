5623019dea11e0d98222a198c51ba1eb68047faf
Patrick Meehan <Meehan.007@hotmail.com>
2021-12-30 21:14:10 -0500

    length works

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,38 @@
-// Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+ 
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+  var length = window.prompt("how long do you want your password to be? you can pick between 4 and 26 characters"); 
+  
+  if (length <= 3 || length >= 27) { 
 
-  passwordText.value = password;
+ window.alert("You need to provide a valid answer! Please try again.");    
+   writePassword();  
+ }
+console.log(length);
 
-}
+    // passwordinfo.charalength(); 
+      var passwordText = document.querySelector("#password"); 
+      passwordText.value = generatePassword(length);
+    
+
+  
+  function generatePassword(length) { 
+  
+    console.log(length); 
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!#$%^&*()+";
+    var password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    } 
+    console.log(password);
+    return password; 
+  } 
+
+}; 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
